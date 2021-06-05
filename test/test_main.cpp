b9baad06ac0c204e17f02c702b339bc28bd71705
ShougoTODA <fiatpanda750@icloud.com>
2021-06-05 13:46:57 +0900

    modify test code.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -14,7 +14,7 @@
 void expand_audio_bits_per_sample_test(void) {
     std::unique_ptr<Application> pApp(new Application);
 
-    BluetoothA2DPSink* a2dpSink = pApp->getA2DPSink();
+    std::unique_ptr<BluetoothA2DPSink> a2dpSink(pApp->getA2DPSink());
 
     a2dpSink->expand_audio_bits_per_sample(I2S_BITS_PER_SAMPLE_8BIT, I2S_BITS_PER_SAMPLE_8BIT);
     a2dpSink->expand_audio_bits_per_sample(I2S_BITS_PER_SAMPLE_16BIT, I2S_BITS_PER_SAMPLE_8BIT);
@@ -44,6 +44,36 @@ void expand_audio_bits_per_sample_test(void) {
     pApp.reset();
 }
 
+void expand_bit_per_sample_test01(void) {
+    std::unique_ptr<Application> pApp(new Application);
+    pApp->setup();
+    pApp->getA2DPSink()->expand_audio_bits_per_sample(I2S_BITS_PER_SAMPLE_16BIT);
+    delay(10 * 1000);
+    //To connect iPhone
+
+    pApp.reset();
+}
+
+void expand_bit_per_sample_test02(void) {
+    std::unique_ptr<Application> pApp(new Application);
+    pApp->setup();
+    pApp->getA2DPSink()->expand_audio_bits_per_sample(I2S_BITS_PER_SAMPLE_24BIT);
+    delay(10 * 1000);
+    //To connect iPhone
+
+    pApp.reset();
+}
+
+void expand_bit_per_sample_test03(void) {
+    std::unique_ptr<Application> pApp(new Application);
+    pApp->setup();
+    pApp->getA2DPSink()->expand_audio_bits_per_sample(I2S_BITS_PER_SAMPLE_32BIT);
+    delay(10 * 1000);
+    //To connect iPhone
+
+    pApp.reset();
+}
+
 void setup() {
     // NOTE!!! Wait for >2 secs
     // if board doesn't support software reset via Serial.DTR/RTS(ボードがsoftwareリセットに対応していない場合に必要なdelay．)
@@ -53,9 +83,17 @@ void setup() {
 
     RUN_TEST(expand_audio_bits_per_sample_test);
 
+    delay(2000);
+    RUN_TEST(expand_bit_per_sample_test01);
+
+    delay(2000);
+    RUN_TEST(expand_bit_per_sample_test02);
+
+    delay(2000);
+    RUN_TEST(expand_bit_per_sample_test03);
+
     UNITY_END();  // stop unit testing
 }
 
 void loop() {
-    ;
 }
