950ef01333298dfe9b3dd3d31aa2dce830f4eefc
ShougoTODA <fiatpanda750@icloud.com>
2021-11-26 23:16:54 +0900

    fix

--- src/Application.h
+++ src/Application.h
@@ -26,103 +26,102 @@ SOFTWARE.
 #include <BluetoothA2DPSink.h>
 #include <esp32-hal-log.h>
 
+constexpr int _BLUE_LED_PORT  = 33;
+constexpr int _GREEN_LED_PORT = 32;
+
 class Application {
-   public:
-    Application() {}
+ public:
+  Application() {
+  }
 
-    ~Application() {}
+  static void avrc_metadata_callback(uint8_t id, const uint8_t* text) {
+    log_i("==> AVRC metadata rsp");
 
-    static void avrc_metadata_callback(uint8_t id, const uint8_t* text) {
-        log_i("==> AVRC metadata rsp");
-        String str((char*)text);
-
-        switch (id) {
-            case 0x01:
-                log_i("==>　曲名：%s", str.c_str());
-                break;
-            case 0x02:
-                log_i("==>　アーティスト：%s", str.c_str());
-                break;
-            case 0x04:
-                log_i("==>　アルバム名：%s", str.c_str());
-                break;
-            case 0x20:
-                log_i("==>　ジャンル：%s", str.c_str());
-                break;
-            default:
-                log_i("==>　Unknown：%s", str.c_str());
-                break;
-        }
+    switch (id) {
+      case 0x01:
+        log_i("==>　曲名：%s", text);
+        break;
+      case 0x02:
+        log_i("==>　アーティスト：%s", text);
+        break;
+      case 0x04:
+        log_i("==>　アルバム名：%s", text);
+        break;
+      case 0x20:
+        log_i("==>　ジャンル：%s", text);
+        break;
+      default:
+        log_i("==>　Unknown：%s", text);
+        break;
     }
+  }
 
-    static void on_data_receive_callback(void) {
-        static int count;
-        if (++count % 100 == 0) {
-            digitalWrite(_BLUE_LED_PORT, HIGH);
-        } else {
-            digitalWrite(_BLUE_LED_PORT, LOW);
-        }
+  static void on_data_receive_callback(void) {
+    static int count;
+    if (++count % 100 == 0) {
+      digitalWrite(_BLUE_LED_PORT, HIGH);
+    } else {
+      digitalWrite(_BLUE_LED_PORT, LOW);
     }
+  }
 #ifdef TEST
-    BluetoothA2DPSink* getA2DPSink(void) {
-        return &_a2dp_sink;
-    }
+  BluetoothA2DPSink* getA2DPSink(void) {
+    return &_a2dp_sink;
+  }
 #endif
-    void setup(void) {
-        pinMode(_BLUE_LED_PORT, OUTPUT);
-        pinMode(_GREEN_LED_PORT, OUTPUT);
+  void setup(void) {
+    pinMode(_BLUE_LED_PORT, OUTPUT);
+    pinMode(_GREEN_LED_PORT, OUTPUT);
 
-        i2s_pin_config_t pin_config = {
-            .bck_io_num   = 26,
-            .ws_io_num    = 22,
-            .data_out_num = 25,
-            .data_in_num  = I2S_PIN_NO_CHANGE  //Use in i2s_pin_config_t for pins which should not be changed
-        };
+    i2s_pin_config_t pin_config = {
+        .bck_io_num   = 26,
+        .ws_io_num    = 22,
+        .data_out_num = 25,
+        .data_in_num  = I2S_PIN_NO_CHANGE  //Use in i2s_pin_config_t for pins which should not be changed
+    };
 
-        //Settings for ES9038Q2M VR1.07 DAC Board(eBay item number:263908779821)
-        i2s_config_t i2s_config = {
-            .mode                 = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-            .sample_rate          = 44100,                       // corrected by info from bluetooth
-            .bits_per_sample      = (i2s_bits_per_sample_t)16,   // set_bits_per_sample()
-            .channel_format       = I2S_CHANNEL_FMT_RIGHT_LEFT,  // 2-channels
-            .communication_format = I2S_COMM_FORMAT_I2S,         // I2S communication format I2S
-            .intr_alloc_flags     = ESP_INTR_FLAG_LEVEL1,        // default interrupt priority
-            .dma_buf_count        = 8,                           // default
-            .dma_buf_len          = 64,                          // default
-            .use_apll             = false,                       // I2S using APLL as main I2S clock, enable it to get accurate clock
-            .tx_desc_auto_clear   = true                         // I2S auto clear tx descriptor if there is underflow condition
-        };
+    //Settings for ES9038Q2M VR1.07 DAC Board(eBay item number:263908779821)
+    i2s_config_t i2s_config = {
+        .mode                 = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+        .sample_rate          = 44100,                       // corrected by info from bluetooth
+        .bits_per_sample      = (i2s_bits_per_sample_t)16,   // set_bits_per_sample()
+        .channel_format       = I2S_CHANNEL_FMT_RIGHT_LEFT,  // 2-channels
+        .communication_format = I2S_COMM_FORMAT_I2S,         // I2S communication format I2S
+        .intr_alloc_flags     = ESP_INTR_FLAG_LEVEL1,        // default interrupt priority
+        .dma_buf_count        = 8,                           // default
+        .dma_buf_len          = 64,                          // default
+        .use_apll             = false,                       // I2S using APLL as main I2S clock, enable it to get accurate clock
+        .tx_desc_auto_clear   = true                         // I2S auto clear tx descriptor if there is underflow condition
+    };
 
-        _a2dp_sink.set_pin_config(pin_config);
-        _a2dp_sink.set_i2s_config(i2s_config);
-        _a2dp_sink.set_bits_per_sample(I2S_BITS_PER_SAMPLE_32BIT);  //for I2S : PCM 44.1K-384K 32BIT
-        _a2dp_sink.set_on_data_received(on_data_receive_callback);
-        _a2dp_sink.set_avrc_metadata_callback(avrc_metadata_callback);
+    _a2dp_sink.set_pin_config(pin_config);
+    _a2dp_sink.set_i2s_config(i2s_config);
+    _a2dp_sink.set_bits_per_sample(I2S_BITS_PER_SAMPLE_32BIT);  //for I2S : PCM 44.1K-384K 32BIT
+    _a2dp_sink.set_on_data_received(on_data_receive_callback);
+    _a2dp_sink.set_avrc_metadata_callback(avrc_metadata_callback);
 
-        _a2dp_sink.start("Riraosan Player", true);
+    _a2dp_sink.start("Riraosan Player", false);
+  }
+
+  void handle(void) {
+    if (_a2dp_sink.isConnected()) {
+      digitalWrite(_GREEN_LED_PORT, HIGH);
+    } else {
+      digitalWrite(_GREEN_LED_PORT, LOW);
     }
 
-    void handle(void) {
-        if (_a2dp_sink.isConnected()) {
-            digitalWrite(_GREEN_LED_PORT, HIGH);
-        } else {
-            digitalWrite(_GREEN_LED_PORT, LOW);
-        }
-
-        switch (_a2dp_sink.get_audio_state()) {
-            case ESP_A2D_AUDIO_STATE_STARTED:
-                break;
-            case ESP_A2D_AUDIO_STATE_STOPPED:
-            case ESP_A2D_AUDIO_STATE_REMOTE_SUSPEND:
-                digitalWrite(_BLUE_LED_PORT, LOW);
-                break;
-            default:
-                break;
-        }
+    switch (_a2dp_sink.get_audio_state()) {
+      case ESP_A2D_AUDIO_STATE_STARTED:
+        break;
+      case ESP_A2D_AUDIO_STATE_STOPPED:
+      case ESP_A2D_AUDIO_STATE_REMOTE_SUSPEND:
+        digitalWrite(_BLUE_LED_PORT, LOW);
+        break;
+      default:
+        break;
     }
+  }
 
-   private:
-    BluetoothA2DPSink _a2dp_sink;
-    constexpr static int _BLUE_LED_PORT  = 33;
-    constexpr static int _GREEN_LED_PORT = 32;
+ private:
+  BluetoothA2DPSink _a2dp_sink;
 };
